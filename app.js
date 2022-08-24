fdab6da60643c5b4c3af7a5159375ceb7962b714
Antonio Rivera <antonio.riverajerez@amcnetworks.com>
2022-08-24 00:24:32 -0600

    adding data models

--- app.js
+++ app.js
@@ -1,16 +1,14 @@
 require('colors');
 const {inquirerMenu,pause} = require('./helpers/inquirer');
+const Tareas = require('./models/tareas');
 
 const main = async() => {
-
     let opt = ''
     do{
         opt = await inquirerMenu();
         console.log({opt});
         await pause();
     }while(opt !== '0')
-
-
 }
 
 main();
---
+++ models/tarea.js
@@ -0,0 +1,16 @@
+//import { v4 as uuidv4 } from 'uuid';
+const {v4: uuidv4 } = require('uuid');
+
+class Tarea{
+
+    id = '';
+    desc = '';
+    completadoEn = null;
+
+    constructor( desc ){
+        this.desc = desc;
+        this.id = uuidv4();
+    }
+}
+
+module.exports = Tarea;
