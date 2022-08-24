36caf5e7466520b31a1ad787c93fc85ab47089b4
Antonio Rivera <antonio.riverajerez@amcnetworks.com>
2022-08-23 20:32:56 -0600

    Initial menu promise

--- app.js
+++ app.js
@@ -1,11 +1,22 @@
 require('colors');
-const {showMenu} = require('./helpers/mensajes');
+const {showMenu, pause} = require('./helpers/mensajes');
 
 const main = async() => {
 
     console.log('Hola mundo'.green.bold);
+    let opt = ''
+    do{
+        opt = await showMenu();
+        console.log({opt});
+        await pause();
+    }while(opt !== '0')
 
+
+    /*
     showMenu();
+    pause();
+    */
+
 }
 
 main();
--- helpers/mensajes.js
+++ helpers/mensajes.js
@@ -1,22 +1,52 @@
+
 require('colors');
 
 const showMenu = () => {
-    console.clear();
-    console.log('=========================='.green);
-    console.log('  Seleccione una opcion  '.yellow.bold);
-    console.log('==========================\n'.green);
-    
-    console.log(`1. Crear Tarea `);
-    console.log(`2. Listar Tareas `);
-    console.log(`3. Listar Tareas Completadas `);
-    console.log(`4. Listar Tareas Pendientes `);
-    console.log(`5. Completar Tarea(s) `);
-    console.log(`6. Borrar Tarea `);
-    console.log(`0. Salir `);
-    console.log('\n');
-    console.log('==========================\n'.green);
+    return new Promise(
+        resolve => {
+            console.clear();
+            console.log('=========================='.green);
+            console.log('  Seleccione una opcion  '.yellow.bold);
+            console.log('==========================\n'.green);
+            console.log(`${'1.'.green} Crear Tarea `);
+            console.log(`${'2.'.green} Listar Tareas `);
+            console.log(`${'3.'.green} Listar Tareas Completadas `);
+            console.log(`${'4.'.green} Listar Tareas Pendientes `);
+            console.log(`${'5.'.green} Completar Tarea(s) `);
+            console.log(`${'6.'.green} Borrar Tarea `);
+            console.log(`${'0.'.green} Salir `);
+            console.log('\n');
+            console.log('==========================\n'.green);
+            console.log('\n');
+            
+            const readLine = require('readline').createInterface({
+                input: process.stdin,
+                output: process.stdout
+            });
+            readLine.question('Selecione una opción: ', (opt) => {
+                readLine.close();
+                resolve(opt);
+            })
+        }
+    )
+}
+
+const pause = () => {
+    return new Promise(
+        resolve => {
+            const readLine = require('readline').createInterface({
+                input: process.stdin,
+                output: process.stdout
+            });    
+            readLine.question(`\nPrecione ${'ENTER'.green.bold} para continuar \n`, (opt) => {
+                readLine.close();
+                resolve();
+            });
+        }
+    )
 }
 
 module.exports = {
-    showMenu
+    showMenu,
+    pause
 }
